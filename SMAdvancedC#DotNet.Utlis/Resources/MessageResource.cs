d513df9f01af54fbd688743171766c00e7f03ea5
sg788 <sumangtm1083@gmail.com>
2024-12-21 22:20:08 +0630

    MessageResource

---
+++ SMAdvancedC#DotNet.Utlis/Resources/MessageResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAdvancedC_DotNet.Utlis.Resources
+{
+    public class MessageResource
+    {
+        public static string Success { get; } = "Success";
+        public static string Fail { get; } = "Fail";
+    }
+}
