e9c280ea4c72d1bc792a822eba85cf17383f2b79
sg788 <sumangtm1083@gmail.com>
2024-12-21 23:29:12 +0630

    [Added]Injections

--- SMAdvancedC#DotNet.RepositoryPattern/Extensions/DependencyInjectionExtension.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Extensions/DependencyInjectionExtension.cs
@@ -1,7 +1,29 @@
-﻿namespace SMAdvancedC_DotNet.RepositoryPattern.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.RepositoryPattern.Extensions
 {
     public static class DependencyInjectionExtension
     {
+        public static IServiceCollection AddDependencies(this IServiceCollection services,WebApplicationBuilder builder)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
 
+
+            services.AddScoped<IBlogRepository, BlogRepository>();
+
+            services.AddControllers()
+                    .AddJsonOptions(opt =>
+                    {
+                        opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    });
+            return services;
+                    
+            
+        }
     }
 }
