545b99404b13fa4975f80b23b59f4dad1b44eb3e
sg788 <sumangtm1083@gmail.com>
2024-12-21 23:40:07 +0630

    Read

---
+++ SMAdvancedC#DotNet.RepositoryPattern/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.RepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        internal readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var lst = await _blogRepository.GetBlogListAsync(pageNo, pageSize, cs);
+            return Ok(lst);
+        }
+    }
+}
