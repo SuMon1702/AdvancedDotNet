70b187634b6f6313c9d629e0c7b1636cc968487f
sg788 <sumangtm1083@gmail.com>
2024-12-21 23:22:40 +0630

    Repositories

--- SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/BlogRepository.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/BlogRepository.cs
@@ -1,7 +1,38 @@
-﻿namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
-{
-    public class BlogRepository:IBlogRepository
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.RepositoryPattern.Models;
+using SMAdvancedC_DotNet.Utlis;
 
+namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly AppDbContext _context;
+        public BlogRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            Result<List<BlogModel>> result;
+           try
+            {
+                var query = _context.TblBlogs.Where(x => x.IsDeleted == false).Skip((pageNo - 1) * pageSize).Take(pageSize);
+                var lst = await query.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent,
+                }).ToListAsync(cs);
+
+                result= Result<List<BlogModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                result = Result<List<BlogModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }
--- SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/IBlogRepository.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/IBlogRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
+﻿using SMAdvancedC_DotNet.RepositoryPattern.Models;
+using SMAdvancedC_DotNet.Utlis;
+
+namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
 {
     public interface IBlogRepository
     {
+        Task<Result<List<BlogModel>>> GetBlogListAsync (int pageNo, int pageSize, CancellationToken cs);
     }
 }
