aa2eb8af47016a64b236510284adec28dfd93fd6
sg788 <sumangtm1083@gmail.com>
2024-12-21 23:29:22 +0630

    -

--- SMAdvancedC#DotNet.RepositoryPattern/Program.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Program.cs
@@ -1,3 +1,5 @@
+using SMAdvancedC_DotNet.RepositoryPattern.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDependencies(builder);
 
 var app = builder.Build();
 
