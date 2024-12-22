ea69ae43f4bcf4e4f11d03da4a790f36b5cea470
sg788 <sumangtm1083@gmail.com>
2024-12-23 01:17:20 +0630

    Blog Repository

---
+++ SMAdvancedC#DotNet.GenericRepository/Persistance/Repositories/BlogRepository.cs
@@ -0,0 +1,11 @@
+﻿using SMAdvancedC_DotNet.Database.Models;
+
+namespace SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories
+{
+    public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
+    {
+        public BlogRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ SMAdvancedC#DotNet.GenericRepository/Persistance/Repositories/IBlogRepository.cs
@@ -0,0 +1,8 @@
+﻿using SMAdvancedC_DotNet.Database.Models;
+
+namespace SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories
+{
+    public interface IBlogRepository : IRepositoryBase<TblBlog>
+    {
+    }
+}
