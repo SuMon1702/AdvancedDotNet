f00fee03e4f184b88813bd21fd75e4f2e68ac566
sg788 <sumangtm1083@gmail.com>
2024-12-23 13:41:42 +0630

    -

--- SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/IBlogRepository.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/IBlogRepository.cs
@@ -8,8 +8,8 @@ namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
         Task<Result<List<BlogModel>>> GetBlogListAsync (int pageNo, int pageSize, CancellationToken cs);
         Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs);
         Task<Result<BlogRequest>> CreateBlogAsync(BlogRequest requestModel, CancellationToken cs);
-        Task<Result<BlogRequest>> UpdateBlogAsync(int id,BlogRequest requestModel, CancellationToken cs);
+        Task<Result<BlogRequest>> UpdateBlogAsync(int blogId,BlogRequest requestModel, CancellationToken cs);
         Task<Result<BlogModel>> DeleteBlogAsync(int blogId, CancellationToken cs);
-        Task UpdateBlogAsync(BlogRequest requestModel, CancellationToken cs);
+        
     }
 }
