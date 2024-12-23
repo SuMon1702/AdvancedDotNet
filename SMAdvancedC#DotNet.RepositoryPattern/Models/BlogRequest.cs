c32c9fa8bc16718f1fc231b79ac8e8a0d61a9bd5
sg788 <sumangtm1083@gmail.com>
2024-12-23 12:23:30 +0630

    Model

---
+++ SMAdvancedC#DotNet.RepositoryPattern/Models/BlogRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SMAdvancedC_DotNet.RepositoryPattern.Models
+{
+    public class BlogRequest
+    {
+        public string BlogTitle { get; set; } = null!;
+
+        public string BlogAuthor { get; set; } = null!;
+
+        public string BlogContent { get; set; } = null!;
+
+        public bool IsDeleted { get; set; }
+    }
+}
