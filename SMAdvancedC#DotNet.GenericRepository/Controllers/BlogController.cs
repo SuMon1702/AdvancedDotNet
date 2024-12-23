5197562ad2e8ac06781b3e1f0474fe7429f8317d
sg788 <sumangtm1083@gmail.com>
2024-12-23 11:37:03 +0630

    -

---
+++ SMAdvancedC#DotNet.GenericRepository/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.GenericRepository.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogRepository _blogRepository;
+    }
+}
