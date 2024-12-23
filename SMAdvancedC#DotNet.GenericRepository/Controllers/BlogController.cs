462edb36e9746a62f3f4c88316399d534c6a8676
sg788 <sumangtm1083@gmail.com>
2024-12-23 12:43:04 +0630

    -

--- SMAdvancedC#DotNet.GenericRepository/Controllers/BlogController.cs
+++ SMAdvancedC#DotNet.GenericRepository/Controllers/BlogController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+using SMAdvancedC_DotNet.shared;
 
 namespace SMAdvancedC_DotNet.GenericRepository.Controllers
 {
@@ -9,5 +11,19 @@ namespace SMAdvancedC_DotNet.GenericRepository.Controllers
     public class BlogController : ControllerBase
     {
         private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var query = _blogRepository.Query().Paginate(pageNo, pageSize);
+            var lst = await query.ToListAsync(cs);
+
+            return Ok(lst);
+        }
     }
 }
