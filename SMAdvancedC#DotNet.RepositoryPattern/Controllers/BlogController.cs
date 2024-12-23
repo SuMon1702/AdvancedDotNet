00d7eb24a48874b92e93fae72ad20c449f7e65be
sg788 <sumangtm1083@gmail.com>
2024-12-23 12:21:24 +0630

    Blog controller

--- SMAdvancedC#DotNet.RepositoryPattern/Controllers/BlogController.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SMAdvancedC_DotNet.RepositoryPattern.Models;
 using SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories;
 
 namespace SMAdvancedC_DotNet.RepositoryPattern.Controllers
@@ -22,6 +23,31 @@ namespace SMAdvancedC_DotNet.RepositoryPattern.Controllers
             return Ok(lst);
         }
 
-       
+        [HttpGet("V1")]
+        public async Task<IActionResult> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var lst = await _blogRepository.GetBlogListAsyncV1(pageNo, pageSize, cs);
+            return Ok(lst);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogAsync(BlogRequest requestModel, CancellationToken cs)
+        {
+            var result = await _blogRepository.CreateBlogAsync(requestModel, cs);
+            return Ok(result);
+        }
+
+        [HttpPut("{BlogId}")]
+        public async Task<IActionResult> UpdateBlogAsync(BlogRequest requestModel, CancellationToken cs)
+        {
+            var result = await _blogRepository.UpdateBlogAsync(requestModel, cs);
+            return Ok(result);
+        }
+
+        [HttpDelete("{BlogId}")]
+        public async Task<IActionResult> DeleteBlogAsync(int blogId, CancellationToken cs)
+        {
+            var result = await _blogRepository.DeleteBlogAsync(blogId, cs);
+            return Ok(result);
+        }
     }
 }
