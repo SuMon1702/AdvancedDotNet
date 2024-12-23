5a4bbb091e26ef7b96fecbafe81d55807d9529e2
sg788 <sumangtm1083@gmail.com>
2024-12-23 12:23:05 +0630

    Paginate

---
+++ SMAdvancedC#DotNet.shared/Entension.cs
@@ -0,0 +1,10 @@
+﻿namespace SMAdvancedC_DotNet.shared
+{
+    public static class Extension
+    {
+        public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> sources, int pageNo, int pageSize)
+        {
+            return sources.Skip((pageNo - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
