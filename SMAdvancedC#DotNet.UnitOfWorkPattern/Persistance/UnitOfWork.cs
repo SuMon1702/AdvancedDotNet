a6bc0c50f817a9dd57775c90fe3f4a6b1f69d57f
sg788 <sumangtm1083@gmail.com>
2024-12-24 23:53:14 +0630

    -

--- SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/UnitOfWork.cs
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/UnitOfWork.cs
@@ -3,17 +3,17 @@ using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
 
 namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         internal readonly AppDbContext _context;
         public IBlogRepository BlogRepository { get; set; }
 
+        RepositoryPattern.Persistance.Repositories.IBlogRepository IUnitOfWork.BlogRepository => throw new NotImplementedException();
+
         public UnitOfWork(AppDbContext context)
         {
             _context = context;
-            BlogRepository ??= new BlogRepository(_context);
-
+            BlogRepository ??= new BlogRepository(context);
         }
-        
     }
 }
