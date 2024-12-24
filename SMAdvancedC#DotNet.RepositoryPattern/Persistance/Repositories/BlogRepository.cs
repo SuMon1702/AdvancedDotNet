6a141645e4bc380db9625ed02107e00211733326
sg788 <sumangtm1083@gmail.com>
2024-12-24 13:09:25 +0630

    -

--- SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/BlogRepository.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Persistance/Repositories/BlogRepository.cs
@@ -18,21 +18,25 @@ namespace SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories
         public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
         {
             Result<List<BlogModel>> result;
-
-            var query = _context.TblBlogs
-            .Where(x => x.IsDeleted == false)
-            .Paginate(pageNo, pageSize);
-
-            var lst = await query.Select(x => new BlogModel()
+            try
             {
-                BlogId = x.BlogId,
-                BlogTitle = x.BlogTitle,
-                BlogAuthor = x.BlogAuthor,
-                BlogContent = x.BlogContent,
-            }).ToListAsync(cs);
+                var query = _context.TblBlogs
+                .Paginate(pageNo, pageSize);
 
-            result = Result<List<BlogModel>>.Success(lst);
-
+                var lst = await query.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent,
+                }).ToListAsync(cs);
+                result = Result<List<BlogModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                    result= Result<List<BlogModel>>.Fail(ex);
+                
+            }
             return result;
         }
 
