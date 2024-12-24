3981c4285cc98ed9d1122db4118857f03527a25f
sg788 <sumangtm1083@gmail.com>
2024-12-24 22:22:28 +0630

    [Added]Blog Repository

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/Repositories/BlogRepository.cs
@@ -0,0 +1,12 @@
+﻿using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.RepositoryPattern.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance.Repositories
+{
+    public class BlogRepository :RepositoryBase<TblBlog> ,IBlogRepository
+    {
+        public BlogRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
