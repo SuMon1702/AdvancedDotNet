c2486f64cb30d73c5dcc3355015aa6b980456543
sg788 <sumangtm1083@gmail.com>
2024-12-24 23:38:14 +0630

    -

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BlogController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var lst = await _unitOfWork.BlogRepository.GetBlogListAsync(pageNo, pageSize, cs);
+            return Ok(lst);
+        }
+    }
+}
