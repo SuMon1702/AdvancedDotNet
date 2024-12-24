652b416b85510607cf2cafa16a09d29c48397d64
sg788 <sumangtm1083@gmail.com>
2024-12-24 23:25:06 +0630

    UnitOfWork

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance
+{
+    public class UnitOfWork
+    {
+        internal readonly AppDbContext _context;
+        public IBlogRepository BlogRepository { get; set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            BlogRepository ??= new BlogRepository(_context);
+
+        }
+        
+    }
+}
