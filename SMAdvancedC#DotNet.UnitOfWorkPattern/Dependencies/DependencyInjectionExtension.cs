6b26d632d9a28c923a33591b2da8b9d4e12c7ff3
sg788 <sumangtm1083@gmail.com>
2024-12-24 22:29:59 +0630

    Injection

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Dependencies/DependencyInjectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Dependencies
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services
+                .AddControllers()
+                .AddJsonOptions(opt =>
+                {
+                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
+
+
+
+            return services;
+        }
+    }
+}
