6fa2f94bacddc74de3ed5b7ba9f68d1815342ce7
sg788 <sumangtm1083@gmail.com>
2024-12-26 16:01:53 +0630

    -

--- SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/Repositories/IBlogRepository.cs
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistance/Repositories/IBlogRepository.cs
@@ -1,5 +1,5 @@
 ﻿using SMAdvancedC_DotNet.Database.Models;
-using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
 namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance.Repositories
 {
     public interface IBlogRepository : IRepositoryBase<TblBlog>
