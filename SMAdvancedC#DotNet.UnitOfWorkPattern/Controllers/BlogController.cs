c5add5e4dd6e9d12ddbbdba05f90a6d80a76cfb0
sg788 <sumangtm1083@gmail.com>
2024-12-31 11:34:48 +0630

    -

--- SMAdvancedC#DotNet.UnitOfWorkPattern/Controllers/BlogController.cs
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Controllers/BlogController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance;
 using SMAdvancedC_DotNet.shared;
 using Microsoft.EntityFrameworkCore;
