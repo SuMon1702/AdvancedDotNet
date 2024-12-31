8ad19f17f990ed3d7c86c6a0b8b7fc66dc2a184d
sg788 <sumangtm1083@gmail.com>
2024-12-31 11:26:38 +0630

    -

--- SMAdvancedC#DotNet.Utlis/Result.cs
+++ SMAdvancedC#DotNet.Utlis/Result.cs
@@ -1,9 +1,4 @@
 ﻿using SMAdvancedC_DotNet.Utlis.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMAdvancedC_DotNet.Utlis
 {
