3038d347135b496a8d0c9967bc943469f3198fd4
sg788 <sumangtm1083@gmail.com>
2024-12-31 11:31:45 +0630

    -

--- SMAdvancedC#DotNet.RepositoryPattern/Handlers/GlobalExceptionalHandler.cs
+++ SMAdvancedC#DotNet.RepositoryPattern/Handlers/GlobalExceptionalHandler.cs
@@ -15,7 +15,6 @@ namespace SMAdvancedC_DotNet.RepositoryPattern.Handlers
                 JsonConvert.SerializeObject(result),
                 cancellationToken
             );
-
             return true;
         }
     }
