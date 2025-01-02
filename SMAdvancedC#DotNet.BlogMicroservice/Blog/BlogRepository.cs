1ab001835b4c46a0c9d46541dcdad84a5c8f2b6c
sg788 <sumangtm1083@gmail.com>
2025-01-02 16:02:44 +0630

    -

---
+++ SMAdvancedC#DotNet.BlogMicroservice/Blog/BlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using SMAdvancedC_DotNet.BlogMicroservice.Features;
+using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.BlogMicroservice.Blog
+{
+    public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
+    {
+        public BlogRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
