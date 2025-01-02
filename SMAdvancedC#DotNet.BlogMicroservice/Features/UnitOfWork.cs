cf474c2d6d7e6c64e5f49d920d1cd2984af52451
sg788 <sumangtm1083@gmail.com>
2025-01-02 16:04:51 +0630

    -

--- SMAdvancedC#DotNet.BlogMicroservice/Features/UnitOfWork.cs
+++ SMAdvancedC#DotNet.BlogMicroservice/Features/UnitOfWork.cs
@@ -1,6 +1,18 @@
-﻿namespace SMAdvancedC_DotNet.BlogMicroservice.Features
+﻿using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.GenericRepository.Persistance.Repositories;
+
+namespace SMAdvancedC_DotNet.BlogMicroservice.Features
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        internal readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            BlogRepository ??= new BlogRepository(_context);
+        }
+
+        public IBlogRepository BlogRepository { get; set; }
     }
 }
