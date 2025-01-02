e3f252a667ceabce2f2715b21a190e8768f5f191
sg788 <sumangtm1083@gmail.com>
2025-01-02 15:36:57 +0630

    -

--- SMAdvancedC#DotNet.BlogMicroservice/Features/RepositoryBase.cs
+++ SMAdvancedC#DotNet.BlogMicroservice/Features/RepositoryBase.cs
@@ -1,6 +1,82 @@
-﻿namespace SMAdvancedC_DotNet.BlogMicroservice.Features
+﻿using Microsoft.EntityFrameworkCore;
+using SMAdvancedC_DotNet.Database.Models;
+using System.Linq.Expressions;
+
+namespace SMAdvancedC_DotNet.BlogMicroservice.Features
 {
-    public class RepositoryBase
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        internal readonly AppDbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public RepositoryBase(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cs = default)
+        {
+            await _dbSet.AddAsync(entity, cs);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs)
+        {
+            await _dbSet.AddRangeAsync(entities,cs);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Dispose()
+        {
+           _context.Dispose();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>>? expression = null)
+        {
+            return expression is not null? _dbSet.Where(expression) : _dbSet.AsQueryable();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cs = default)
+        {
+            await _context.SaveChangesAsync(cs);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+        }
     }
 }
