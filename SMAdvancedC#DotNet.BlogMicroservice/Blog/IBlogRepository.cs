edc442240161785e2855d1668d1b204e52368805
sg788 <sumangtm1083@gmail.com>
2025-01-02 16:04:27 +0630

    -

---
+++ SMAdvancedC#DotNet.BlogMicroservice/Blog/IBlogRepository.cs
@@ -0,0 +1,10 @@
+﻿using SMAdvancedC_DotNet.BlogMicroservice.Features;
+using SMAdvancedC_DotNet.Database.Models;
+
+namespace SMAdvancedC_DotNet.BlogMicroservice.Blog
+{
+    public interface RepositoryBase:IRepositoryBase<TblBlog>
+    {
+        
+    }
+}
