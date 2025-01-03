0c270c9d4f49572beca6e77bd42a480c3da7f174
sg788 <sumangtm1083@gmail.com>
2025-01-03 16:04:16 +0630

    -

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Model/BlogRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Model
+{
+    public class BlogRequest
+    {
+        public string BlogTitle { get; set; } = null!;
+
+        public string BlogAuthor { get; set; } = null!;
+
+        public string BlogContent { get; set; } = null!;
+
+        public bool IsDeleted { get; set; }
+    }
+}
