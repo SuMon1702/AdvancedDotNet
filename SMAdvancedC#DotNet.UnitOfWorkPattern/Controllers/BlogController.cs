3e0c9163777148168dd8e6d7054bd77b1a6582b6
sg788 <sumangtm1083@gmail.com>
2025-03-21 21:23:57 +0630

    -

--- SMAdvancedC#DotNet.UnitOfWorkPattern/Controllers/BlogController.cs
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Controllers/BlogController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SMAdvancedC_DotNet.UnitOfWorkPattern.Persistance;
+using SMAdvancedC_DotNet.UnitOfWorkPattern.Persistence;
 using SMAdvancedC_DotNet.shared;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,11 +18,11 @@ public class BlogController : ControllerBase
 
 
     #region GetBlogsAsync
-
     [HttpGet]
-    public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cs)
+    public async Task<IActionResult> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
     {
-        var query = _unitOfWork.BlogRepository.Query().Paginate(pageNo, pageSize);
+        var query = _unitOfWork.BlogRepository.Query()
+               .Paginate(pageNo, pageSize);
         var lst = await query.ToListAsync(cs);
 
         return Ok(lst);
