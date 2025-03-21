dbd6f12218b275c78a08c5cc9864e2165ce26480
sg788 <sumangtm1083@gmail.com>
2025-03-21 21:25:22 +0630

    -

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistence/Repositories/IBlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.UnitOfWorkPattern.Model;
+using SMAdvancedC_DotNet.Utlis;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistence.Repositories;
+
+public interface IBlogRepository : IRepositoryBase<TblBlog>
+{
+    Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs);
+   // Task<Result<BlogModel>> GetBlogByIdAsync(int blogId, CancellationToken cs);
+
+
+}
