08ebe62746050a7a041cf13edeb4ce2a0892e909
sg788 <sumangtm1083@gmail.com>
2025-03-21 21:25:09 +0630

    feat:GetBlogListAsync

---
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistence/Repositories/BlogRepository.cs
@@ -0,0 +1,44 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.EntityFrameworkCore;
+using MySqlX.XDevAPI.Common;
+using SMAdvancedC_DotNet.Database.Models;
+using SMAdvancedC_DotNet.shared;
+using SMAdvancedC_DotNet.UnitOfWorkPattern.Model;
+using SMAdvancedC_DotNet.Utlis;
+
+namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistence.Repositories
+{
+    public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
+    {
+        public BlogRepository(AppDbContext context)
+            : base(context) { }
+
+        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo,int pageSize, CancellationToken cs)
+        {
+            Result<List<BlogModel>> response;
+            try
+            {
+                var query = _context.TblBlogs.Paginate(pageNo, pageSize);
+
+                var lst = await query.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent,
+                }).ToListAsync(cs);
+                response = Result<List<BlogModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                response = Result<List<BlogModel>>.Fail(ex);
+            }
+            return response;
+        }
+
+    }
+
+
+
+    }
+
