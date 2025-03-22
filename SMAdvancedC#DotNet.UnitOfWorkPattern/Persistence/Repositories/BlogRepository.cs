3a5ec82ca8af24424ef7b6966fbbebd919b5b543
sg788 <sumangtm1083@gmail.com>
2025-03-22 23:21:35 +0630

    feat:GetBlogByIdAsync

--- SMAdvancedC#DotNet.UnitOfWorkPattern/Persistence/Repositories/BlogRepository.cs
+++ SMAdvancedC#DotNet.UnitOfWorkPattern/Persistence/Repositories/BlogRepository.cs
@@ -13,7 +13,7 @@ namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistence.Repositories
         public BlogRepository(AppDbContext context)
             : base(context) { }
 
-        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo,int pageSize, CancellationToken cs)
+        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
         {
             Result<List<BlogModel>> response;
             try
@@ -36,9 +36,37 @@ namespace SMAdvancedC_DotNet.UnitOfWorkPattern.Persistence.Repositories
             return response;
         }
 
+        public async Task<Result<BlogModel>> GetBlogByIdAsync(int blogId, CancellationToken cs)
+        {
+            Result<BlogModel> response;
+            try
+            {
+                var query = _context.TblBlogs.Where(x => x.BlogId == blogId);
+                var blog = await query.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent,
+                }).FirstOrDefaultAsync(cs);
+                if (blog == null)
+                {
+                    response = Result<BlogModel>.Fail("Blog not found.");
+                }
+                else
+                {
+                    response = Result<BlogModel>.Success(blog);
+                }
+            }
+            catch (Exception ex)
+            {
+                response = Result<BlogModel>.Fail(ex);
+            }
+            return response;
+
+        }
+
     }
 
-
-
-    }
+}
 
